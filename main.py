1c642f5066641a6293ec7bcadd223bb39b416829
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-04-05 13:17:41 +0530

    Added player rocet on screen

--- main.py
+++ main.py
@@ -18,11 +18,25 @@ clock = pygame.time.Clock()
 
 gameover = False
 
+class Player(object):
+    def __init__(self):
+        self.img = playerRocket
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.x = s_width // 2
+        self.y = s_height // 2
+
+    def draw(self, win):
+        win.blit(self.img, [self.x, self.y, self.width, self.height])
+        
+
 def redrawGameWindow():
     win.blit(bg, (0,0))
-
+    player.draw(win)
     pygame.display.update()
 
+
+player = Player()
 run = True
 while run:
     clock.tick(60)
